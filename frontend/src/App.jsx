66d2d08e4a1d17d59cd05bd887f56f13b855a0cf
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-10-13 09:11:56 +0530

    Initial Commit - IntelliLMS setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/signup" element={<Signup/>}/>
+    <Route path="/login" element={<Login/>}/>
+   </Routes>
+   </>
+  )
+}
+
+export default App
