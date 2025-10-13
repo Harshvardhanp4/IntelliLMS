72475b536007eb2e96f93f20fc69f96cda4c11e7
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-10-13 10:44:04 +0530

    Frontend Signup Signin - IntelliLMS setup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+import logo from "../assets/ims_logo.png"
+import google from "../assets/google.png"
 
 function Login(){
     return (
-        <div className='text-2xl'>
-            Login Page 
-        </div>
+        <div className='bg-[#dddbdb] w-[100vw] h-[100vh] flex items-center justify-center'>
+                   <form className='w-[90%] md:w-200 h-150 bg-white shadow-xl rounded-2xl flex'>
+                        {/* left div */}
+        
+                        <div className='md:w-[50%] w-[100%] h-[100%] flex flex-col items-center justify-center gap-3'>
+                            <div>
+                            <h1 className='font-semibold text-black text-2xl'>Welcome back!</h1>
+                            <h2 className='text-[#999797] text-[18px]'>Login in your account</h2>
+                            </div>
+        
+                          
+        
+                            {/* email */}
+                             <div className=' flex flex-col gap-1 w-[80%] items-start justify-center px-3'>
+                            <label htmlFor="email" className='font-semibold'>Email</label>
+                            <input id='email' type="text" className='border-1 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px]' placeholder='Your Email'/>
+                            </div>
+        
+                            {/* password */}
+                             <div className=' flex flex-col gap-1 w-[80%] items-start justify-center px-3'>
+                            <label htmlFor="password" className='font-semibold'>Password</label>
+                            <input id='password' type="password" className='border-1 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px]' placeholder='Your Password'/>
+                            </div>
+        
+                           
+        
+                            {/* button */}
+                            <button type="button" class="w-[80%] h-[40px] text-white bg-black hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 cursor-pointer">Login</button>
+                            <div className='w-[80%] flex items-center gap-2'>
+                                <div className='w-[25%] h-[0.5px] bg-[#c4c4c4]'></div>
+                                <div className='w-[50%] text-[15px] text-[#6f6f6f] flex items-center justify-center'>or continue</div>
+                                <div className='w-[25%] h-[0.5px] bg-[#c4c4c4]'></div>
+                            </div>
+        
+                            <div className='w-[80%] h-[40px] border-1 border-black rounded-[5px]n flex items-center justify-center '>
+                                <img src={google} className='w-[40px] -mr-2'alt="" />
+                                <span className='cursor-pointer'>oogle</span>
+                            </div>
+        
+        
+        
+                        </div>
+         
+                        {/* right div */}
+                        <div className='w-[50%] h-[100%] rounded-r-2xl bg-black md:flex items-center justify-center flex-col hidden'>
+                            <img src={logo} alt="IntelliLMS Logo" className='w-30 shadow-2xl' />
+                            <span className='text-2xl text-white'>IntelliLMS</span>
+        
+                        </div>
+                   </form>
+                </div>
     )
 }
 
