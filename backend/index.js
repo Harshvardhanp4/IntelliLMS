938639f74edcf216e3cbe3c08c8b9e61dda545cc
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-11-01 11:21:22 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 
-app.use(cors())
+app.use(cors({
+    origin: "*"
+}));
 app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
 app.use("/api/course", courseRouter)
