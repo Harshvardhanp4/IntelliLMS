4d229f3e625db485cbee9cfa1a9161a9a27adeac
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-11-01 11:01:09 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDb from './config/connectdb.js';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/authRoute.js';
+import cors from "cors";
 import userRouter from './routes/userRoute.js';
 import courseRouter from './routes/courseRoute.js';
 import paymentRouter from './routes/paymentRoute.js';
@@ -13,6 +14,8 @@ const port = process.env.PORT
 const app = express();
 app.use(express.json());
 app.use(cookieParser())
+
+app.use(cors())
 app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
 app.use("/api/course", courseRouter)
