3e8576fb561e1745baed6b8d408c068a75a42915
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-11-02 21:48:21 +0530

    Small changes + trying google auth fix

--- frontend/src/pages/Logos.jsx
+++ frontend/src/pages/Logos.jsx
@@ -6,23 +6,23 @@ import { FaUsers } from "react-icons/fa";
 function Logos() {
   return (
     <div className="w-[100vw] min-h-[90px] flex items-center justify-center flex-wrap gap-4 md:mb-[50px]">
-      <div className="flex items-center justify-center gap-2 px-5 py-3 rounded-3xl bg-gray-200 cursor-pointer text-[#03394b]">
+      <div className="flex items-center justify-center gap-2 px-5 py-3 rounded-3xl bg-gray-200  text-[#03394b]">
         <MdCastForEducation className="w-[35px] h-[35px] fill-[#03394b]" />
         20k+ Online Courses
       </div>
-      <div className="flex items-center justify-center gap-2 px-5 py-3 rounded-3xl bg-gray-200 cursor-pointer text-[#03394b]">
+      <div className="flex items-center justify-center gap-2 px-5 py-3 rounded-3xl bg-gray-200  text-[#03394b]">
         <SiOpenaccess className="w-[35px] h-[35px] fill-[#03394b]" />
         Lifetime Access
       </div>
-      <div className="flex items-center justify-center gap-2 px-5 py-3 rounded-3xl bg-gray-200 cursor-pointer text-[#03394b]">
+      <div className="flex items-center justify-center gap-2 px-5 py-3 rounded-3xl bg-gray-200  text-[#03394b]">
         <FaSackDollar className="w-[35px] h-[35px] fill-[#03394b]" />
         Value for money
       </div>
-      <div className="flex items-center justify-center gap-2 px-5 py-3 rounded-3xl bg-gray-200 cursor-pointer text-[#03394b]">
+      <div className="flex items-center justify-center gap-2 px-5 py-3 rounded-3xl bg-gray-200  text-[#03394b]">
         <BiSupport className="w-[35px] h-[35px] fill-[#03394b]" />
         Lifetime Support
       </div>
-      <div className="flex items-center justify-center gap-2 px-5 py-3 rounded-3xl bg-gray-200 cursor-pointer text-[#03394b]">
+      <div className="flex items-center justify-center gap-2 px-5 py-3 rounded-3xl bg-gray-200  text-[#03394b]">
         <FaUsers className="w-[35px] h-[35px] fill-[#03394b]" />
         Community Support
       </div>
--- frontend/utils/firebase.js
+++ frontend/utils/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
@@ -18,5 +18,6 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app)
 const provider = new GoogleAuthProvider()
+provider.setCustomParameters({ prompt: 'select_account' });
 
-export {auth, provider}
+export { auth, provider }
