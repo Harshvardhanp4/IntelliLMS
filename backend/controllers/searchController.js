468e59ec061b1563705dc3e99d612f673f8e7dbc
Harshvardhanp4 <harshupatil.224@gmail.com>
2026-05-12 09:10:38 +0530

    changed gemini model

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -34,7 +34,7 @@ export const searchWithAi = async (req, res) => {
         `
 
         const response = await ai.models.generateContent({
-            model: "gemini-2.5-flash",
+            model: "gemini-1.5-flash",
             contents: prompt,
         })
 
